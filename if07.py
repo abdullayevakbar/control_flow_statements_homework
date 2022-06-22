8ca4d7001ddd6190ae1e827494aac6841528fb22
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-06-22 16:02:55 +0500

    IF

--- if07.py
+++ if07.py
@@ -12,4 +12,15 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if(a > 0):
+        if(a % 2 == 1):
+            return "positive odd number"
+        if(a % 2 == 0):
+            return "positive even number"
+    if(a < 0):
+        a = abs(a)
+        if(a % 2 == 1):
+            return "negative odd number"
+        if(a % 2 == 0):
+            return "negative even number"
+    return "the number is zero"
